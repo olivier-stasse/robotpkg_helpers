d885e8c250c191d7945cf0eb819faea299e8d262
Olivier Stasse <ostasse@laas.fr>
2019-02-21 07:56:39 +0100

    [module] Reflects in __init__.py previous changes Bad commit: two changes which are not related and should have been done in other commits.

--- robotpkg_helpers/__init__.py
+++ robotpkg_helpers/__init__.py
@@ -1,4 +1,5 @@
-from .test_deployment import RobotpkgTests
+from .testing_deployment import RobotpkgTests
 from .dockerfile import RobotpkgGenerateDockerFile
 from .src_introspection import RobotpkgSrcIntrospection,RobotpkgPackage
 from .utils import execute,execute_call,build_test_rc_robotpkg_vars
+from .laas_workspace import RobotpkgLaasWorkspace
