190abed4eb1a2b4c122000835322fe2ac283e6a7
Olivier Stasse <ostasse@laas.fr>
2019-02-25 18:36:15 +0100

    [utils] Populate robotpkg_mng_vars

--- robotpkg_helpers/utils.py
+++ robotpkg_helpers/utils.py
@@ -36,17 +36,45 @@ def execute_call(bashCommand, debug=0):
         print("execute bash command: "+bashCommand)
     return subprocess.call(bashCommand,shell=True)
 
-def build_test_rc_robotpkg_vars():
+def build_test_rc_robotpkg_vars(ROBOTPKG_ROOT=None):
     """ Build a dictionnary of basic robotpkg variables for a standard test_rc setup
     """
     import os
-    env_vars=os.environ.copy()
-    root_devel_src=env_vars["HOME"]+'/devel-src/robotpkg_ws'
-    user=env_vars["USER"]
+    if ROBOTPKG_ROOT==None:
+        env_vars=os.environ.copy()
+        ROBOTPKG_ROOT=env_vars["HOME"]+'/devel-src/robotpkg-test-rc'
+        user=env_vars["USER"]
+
 
     robotpkg_vars={}
-    robotpkg_vars['ROOT']=root_devel_src+'/robotpkg-test-rc'
+    robotpkg_vars['ROOT']=ROBOTPKG_ROOT
     robotpkg_vars['SRC']=robotpkg_vars['ROOT']+'/robotpkg'
+    robotpkg_vars['ROOT']=robotpkg_vars['ROOT']+'/install'
     robotpkg_vars['DISTFILES']=robotpkg_vars['SRC']+'/distfiles'
     return robotpkg_vars
+
+def add_robotpkg_mng_variables(anObject, ROBOTPKG_MNG_ROOT=None):
+    """ This function adds robotpkg_mng_vars to the object based on ROBOTPKG_MNG_ROOT
+    if provided.
+
+    All of this is done for intermediate build when working on release candidates
+    and speed up deployment tests.
+    
+    TODO: Test over a solution an integrative solution with dockerfile and 
+    intermediate binary build.
+    """
+    if ROBOTPKG_MNG_ROOT==None:
+        anObject.ROBOTPKG_MNG_ROOT='/integration_tests'
+    else:
+        anObject.ROBOTPKG_MNG_ROOT=ROBOTPKG_MNG_ROOT
+
+    anObject.robotpkg_mng_vars={}
+    anObject.robotpkg_mng_vars['ROOT'] = anObject.ROBOTPKG_MNG_ROOT
+    anObject.robotpkg_mng_vars['ARCH_DISTFILES']=anObject.robotpkg_mng_vars['ROOT']+'/arch_distfiles'
+    anObject.robotpkg_mng_vars['RAMFS_MNT_PT']=anObject.robotpkg_mng_vars['ROOT']+'/robotpkg-test-rc'
+    anObject.robotpkg_mng_vars['ROBOTPKG_ROOT']=anObject.robotpkg_mng_vars['RAMFS_MNT_PT']
+    anObject.robotpkg_mng_vars['ARCHIVES']=anObject.robotpkg_mng_vars['ROOT']+'/archives'
+    anObject.robotpkg_mng_vars['ROBOTPKG_BASE']=anObject.robotpkg_mng_vars['RAMFS_MNT_PT']+'/install'
+    anObject.robotpkg_mng_vars['ROBOTPKG_SRC']=anObject.robotpkg_mng_vars['RAMFS_MNT_PT']+'/robotpkg'
+
 
