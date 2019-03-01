8f9d3b0142091fbe74c93aa50a2dcba6be48ba25
Olivier Stasse <ostasse@laas.fr>
2019-03-01 18:02:52 +0100

    [tests] Fix the installation dir. And change the directory to be tested.

--- robotpkg_helpers/tests/test_rc_other_path.py
+++ robotpkg_helpers/tests/test_rc_other_path.py
@@ -3,17 +3,19 @@ import os
 from robotpkg_helpers import RobotpkgTests, build_test_rc_robotpkg_vars
 
 
-arch_release_candidates= [ ('sot-core-v3','master'),
+arch_release_candidates= [ ('dynamic-graph-v3','devel'),
+                           ('sot-core-v3','master'),
                            ('py-sot-core-v3','master'),
                            ('sot-dynamic-pinocchio-v3','master'),
                            ('py-sot-dynamic-pinocchio-v3','master'),
                            ('roscontrol-sot','master'),
+                           ('sot-torque-control','master'),
                            ('sot-talos','master')
-                           ]
+]
 
 robotpkg_vars = build_test_rc_robotpkg_vars()
 dist_files_path=robotpkg_vars['DISTFILES']
-arpgtestrc =RobotpkgTests("/integration_tests")
+arpgtestrc =RobotpkgTests("/integration_tests/robotpkg-test-rc")
 
 # Perform the deployment in arpgtestrc
 if arpgtestrc.perform_test_rc(arch_release_candidates,dist_files_path):
