50c0c8c863b756b1dafdff3aedec8837a63d9e22
Olivier Stasse <ostasse@laas.fr>
2019-03-12 22:44:34 +0100

    [robotpkg_helpers] Remove laas_workspace reference.

--- robotpkg_helpers/__init__.py
+++ robotpkg_helpers/__init__.py
@@ -2,6 +2,5 @@ from .testing_deployment import RobotpkgTests
 from .dockerfile import RobotpkgGenerateDockerFile
 from .src_introspection import RobotpkgSrcIntrospection,RobotpkgPackage
 from .utils import execute,execute_call,build_test_rc_robotpkg_vars
-from .laas_workspace import RobotpkgLaasWorkspace
 from .handling_imgs import HandlingImgs
 from .release_candidate import RobotpkgReleaseCandidate
