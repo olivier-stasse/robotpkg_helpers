3afc65f87a3c22fac77b25bae1202f39b52b5d81
Olivier Stasse <ostasse@laas.fr>
2019-04-11 16:32:48 +0200

    [tools] Add completion for restore dir (in /integration/robotpkg-test-rc)

--- tools/rpkgh_restore_dir.py
+++ tools/rpkgh_restore_dir.py
@@ -1,8 +1,78 @@
 #!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK
+
 import os
 import sys
+import argcomplete, argparse
+from pathlib import Path
+
 from robotpkg_helpers import HandlingImgs
-import argparse
+
+class TestCompleter(object):
+    def __init__(self,choices):
+        self.choices = choices
+
+    def __call__(self, parsed_args, **kwargs):
+        argcomplete.warn("toto")
+        argcomplete.warn(parsed_args)
+        argcomplete.warn(self.choices)
+        return self.choices
+
+class FilenameCompleter(object):
+    def __init__(self):
+        argcomplete.warn("FilenameCompleter: init")
+        p = Path('/integration_tests/archives2/')
+        list_tgz = list(p.glob('**/*.tgz'))
+        list_tgz=sorted(list_tgz)
+        self.choices = list_tgz
+        
+    def __call__(self, parsed_args, **kwargs):
+        lstr = parsed_args.rpkgmngroot+'/'+ parsed_args.sub_archives
+        p = Path(lstr)
+        list_tgz = list(p.glob('**/*.tgz'))
+        list_tgz=sorted(list_tgz)
+        final_choices=[]
+        for atgz in list_tgz:
+            final_choices.append(atgz.name)
+        return final_choices
+                
+    
+class RamfsmntptCompleter(object):
+
+    def __init__(self,choices):
+        argcomplete.warn('RamfsmntptCompleter:init')
+        self.choices = choices
+        
+    def __call__(self, parsed_args, **kwargs):
+        argcomplete.warn("going through call")
+        # p = Path(parser_args.rpkgmngroot+'/')
+        # argcomplete.warn(p)
+        # self.choices=['test','test2']        
+        # for child in p.iterdir():
+        #     self.choices.append(child)
+        #     argcomplete.warn(child)
+        return self.choices
+
+class DirrpkgmngrootCompleter(object):
+
+    def __init__(self):
+        argcomplete.warn("DirrpkgmngrootCompleter")
+        
+    def __call__(self, parsed_args, **kwargs):
+        #argcomplete.warn("DirrpkgmngrootCompleter")
+        lstr=parsed_args.rpkgmngroot+'/'
+        #argcomplete.warn(lstr)
+        choices=[]
+        p = Path(lstr)
+        for x in p.iterdir():
+            #argcomplete.warn(str(x))
+            if x.is_dir():
+                choices.append(x.name)
+        return choices
+    
+# print(parsed_args)
+# argcomplete.warn("toto")
+# return "test"
 
 class RpkghRestoreDir():
 
@@ -13,24 +83,31 @@ class RpkghRestoreDir():
 
     def handle_options(self):
         parser = argparse.ArgumentParser(description='Restore an install/robotpkg directory.')
-        parser.add_argument('filename', metavar="filename",
+
+        parser.add_argument('filename',
+                            metavar="filename",
                             action="store", nargs=1,
-                            help='File to uncompress')
+                            help='File to uncompress'
+        ).completer = FilenameCompleter()
 
         parser.add_argument("-m", "--ramfsmntpt", dest="sub_ramfsmntpt", action="store",
-                            default="robotpkg-test-rc",nargs=1,
-                            help='Subdirectory in ROBOTPKG_MNG_ROOT where to uncompress \n(default:robotpkg-test-rc)')
-
+                             default="robotpkg-test-rc",nargs=1,
+                            help='Subdirectory in ROBOTPKG_MNG_ROOT where to uncompress \n(default:robotpkg-test-rc)'
+        ).completer = TestCompleter(('robotpkg-test-rc','RR'))
+        
         parser.add_argument("-r", "--rpkgmngroot", dest="rpkgmngroot", action="store",
                             default="/integration_tests", nargs=1,
-                            help='Directory corresponding to ROBOTPKG_MNG_ROOT \n(default: /integration_tests)')
+                            help='Directory corresponding to ROBOTPKG_MNG_ROOT \n(default: /integration_tests)'
+        ).completer = TestCompleter(('toto','tata'))
 
         parser.add_argument("-a", "--archives", dest='sub_archives', action='store',
                             default='archives',nargs=1,
-                            help='Subdirectory in ROBOTPKG_MNG_ROOT where archives are stored\n (default: archives)')
+                            help='Subdirectory in ROBOTPKG_MNG_ROOT where archives are stored\n (default: archives)'
+        ).completer = DirrpkgmngrootCompleter()
 
+        argcomplete.autocomplete(parser)
         parser.parse_args(namespace=self)
-
+        
         if isinstance(self.sub_ramfsmntpt,list):
             self.sub_ramfsmntpt=self.sub_ramfsmntpt[0]
 
@@ -48,5 +125,9 @@ class RpkghRestoreDir():
 
         aHandlingImgs.restore_from_backup_dir(tar_file_name=filename)
 
+
 if __name__ == "__main__":
+        
     aRpkghRestoreDir = RpkghRestoreDir()
+    
+
